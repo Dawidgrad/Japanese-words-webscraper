53454eb34812954e291804b25c84553385e77b0f
Dawid Grad <dawidgrad.work@gmail.com>
2020-07-04 22:48:50 +0100

    Get the HTML content of the website

--- webscraper.py
+++ webscraper.py
@@ -1,10 +1,20 @@
-# msg = "Hello world"
-# print(msg)
-# input("Press Enter to continue...")
-
 from bs4 import BeautifulSoup
 import requests
 
 baseUrl = "https://iknow.jp/courses/"
-print(baseUrl)
+subpages = list(range(566921, 566933))
+subpages += list(range(594768, 594781))
+subpages += list(range(615865, 615878))
+subpages += list(range(615947, 615960)) 
+subpages += list(range(616077, 616087))
+subpages += list(range(598422, 598435))
+
+# for i in subpages:
+fullUrl = baseUrl + str(566921)
+print(fullUrl + "\n")
+html_content = requests.get(fullUrl).text
+soup = BeautifulSoup(html_content, "lxml")
+
+print(soup.prettify())
+
 input("Press Enter to continue...")
