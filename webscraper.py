57adffc6334db10c7ae068e93b4843512f751253
Dawid Grad <dawidgrad.work@gmail.com>
2020-07-04 21:44:02 +0100

    Initial commit

---
+++ webscraper.py
@@ -0,0 +1,10 @@
+# msg = "Hello world"
+# print(msg)
+# input("Press Enter to continue...")
+
+from bs4 import BeautifulSoup
+import requests
+
+baseUrl = "https://iknow.jp/courses/"
+print(baseUrl)
+input("Press Enter to continue...")
